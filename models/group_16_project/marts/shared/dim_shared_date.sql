8dafbd20627e607d6ae7642993f962c742dcf6b3
Victor Chen <vchen82@gmail.com>
2026-05-04 02:24:24 +0000

    updated dim_shared_date.sql

--- models/group_16_project/marts/shared/dim_shared_date.sql
+++ models/group_16_project/marts/shared/dim_shared_date.sql
@@ -1,43 +1,36 @@
-WITH dates AS (
-   -- Get dates (dates, no time included) from 311 requests
-   SELECT DISTINCT 
-    CAST(created_date AS DATE) AS full_date,
-    FORMAT_TIME('$H:$M', CAST(created_date AS TIME)) AS hour_of_day
-   FROM {{ ref('stg_nyc_311_vehicle_complaints') }}
-   WHERE created_date IS NOT NULL
+{{ config(materialized='table') }}
 
-   UNION DISTINCT
+with all_dates as (
 
-   -- Get dates from vehicle crashes
-   SELECT DISTINCT 
-    CAST(crash_date AS DATE) AS full_date,
-    FORMAT_TIME('%H',PARSE_TIME('%H:%M', crash_time)) AS hour_of_day,
-   FROM {{ ref('stg_nyc_vehicle_crashes') }}
-   WHERE crash_date IS NOT NULL
+    -- 311 dates (date only)
+    select distinct
+        cast(created_date as date) as full_date
+    from {{ ref('stg_nyc_311_vehicle_complaints') }}
+    where created_date is not null
+
+    union distinct
+
+    -- crash dates (date only)
+    select distinct
+        cast(crash_date as date) as full_date
+    from {{ ref('stg_nyc_vehicle_crashes') }}
+    where crash_date is not null
 ),
 
-final AS (
-
-    SELECT
-        {{ dbt_utils.generate_surrogate_key(['full_date']) }} AS date_key,
+final as (
+    select
+        {{ dbt_utils.generate_surrogate_key(['full_date']) }} as date_key,
 
         full_date,
-        EXTRACT(YEAR FROM full_date) AS year,
-        EXTRACT(MONTH FROM full_date) AS month,
-        EXTRACT(DAY FROM full_date) AS day,
-        FORMAT_DATE('%A', full_date) AS day_of_week,
-        FORMAT_DATE('%B', full_date) AS month_name,
-        EXTRACT(DAYOFWEEK FROM full_date) IN (1, 7) AS is_weekend,
-        hour_of_day,
-          
-     CASE
-       WHEN hour_of_day < "12:00" THEN "AM"
-       ELSE "PM"
-    END AS time_of_day
-    
+        extract(year from full_date) as year,
+        extract(month from full_date) as month,
+        extract(day from full_date) as day,
+        format_date('%A', full_date) as day_of_week,
+        format_date('%B', full_date) as month_name,
+        extract(dayofweek from full_date) in (1, 7) as is_weekend
 
-    FROM dates
-
+    from all_dates
 )
 
-SELECT * FROM final
+select *
+from final
